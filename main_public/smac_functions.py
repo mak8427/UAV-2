34f22f9cc4d731166db0aba007e6b219244e21a4
Davide <davide.mattioli.2001@gmail.com>
2025-03-11 11:17:45 +0100

    Eastern and norting Function fixed

--- main_public/smac_functions.py
+++ main_public/smac_functions.py
@@ -89,79 +89,57 @@ def xy_np(transform, rows, cols, offset='center'):
 def latlon_to_utm32n_series(lat_deg, lon_deg):
     """
     Convert geographic coordinates (lat, lon in degrees, WGS84)
-    to UTM Zone 32N (EPSG:32632) using series expansions.
+    to UTM Zone 32N (EPSG:32632) using the standard UTM formulas.
 
     Returns:
       (easting, northing) in meters.
     """
-    # WGS84 constants
-    a = 6378137.0                     # semi-major axis
-    f = 1 / 298.257223563             # flattening
-    e2 = 2*f - f*f                    # eccentricity squared
+    # WGS84 ellipsoid constants
+    a = 6378137.0                       # semi-major axis (meters)
+    f = 1 / 298.257223563               # flattening
+    e2 = 2*f - f**2                     # eccentricity squared
     e = math.sqrt(e2)
 
     # UTM parameters for Zone 32N
     k0 = 0.9996
-    lambda0_deg = 9.0  # central meridian
-    E0 = 500000.0
-    N0 = 0.0           # for northern hemisphere
+    E0 = 500000.0                       # false easting
+    N0 = 0.0                            # false northing (northern hemisphere)
+    lambda0 = math.radians(9.0)         # central meridian for Zone 32N (9°E)
 
-    # 1) Compute n, A, alpha_i
-    n = f / (2 - f)
+    # Convert input latitude and longitude from degrees to radians
+    phi = math.radians(lat_deg)
+    lam = math.radians(lon_deg)
 
-    # A factor (truncated series for (1 + n^2/4 + n^4/64 + ...))
-    # We'll keep up to n^4 for typical accuracy.
-    A_bar = (1 + (n**2)/4 + (n**4)/64)
-    A = (a / (1 + n)) * A_bar
+    # Compute auxiliary values
+    N_val = a / math.sqrt(1 - e2 * math.sin(phi)**2)
+    T = math.tan(phi)**2
+    # Second eccentricity squared
+    ep2 = e2 / (1 - e2)
+    C = ep2 * math.cos(phi)**2
+    A = (lam - lambda0) * math.cos(phi)
 
-    # Some alpha_i expansions (truncated to 3 terms)
-    alpha_1 = 0.5*n - (2/3)*n**2 + (37/96)*n**3
-    alpha_2 = (1/48)*n**2 + (1/15)*n**3
-    alpha_3 = (17/480)*n**3  # can add more if needed
+    # Meridional arc length (M)
+    M = a * (
+          (1 - e2/4 - 3*e2**2/64 - 5*e2**3/256) * phi
+        - (3*e2/8 + 3*e2**2/32 + 45*e2**3/1024) * math.sin(2*phi)
+        + (15*e2**2/256 + 45*e2**3/1024) * math.sin(4*phi)
+        - (35*e2**3/3072) * math.sin(6*phi)
+    )
 
-    # 2) Convert lat, lon to radians; shift by central meridian
-    lat = math.radians(lat_deg)
-    lon = math.radians(lon_deg)
-    lambda0 = math.radians(lambda0_deg)
+    # Calculate Easting and Northing using standard UTM series formulas
+    easting = E0 + k0 * N_val * (
+          A
+        + (1 - T + C) * A**3 / 6
+        + (5 - 18*T + T**2 + 72*C - 58*ep2) * A**5 / 120
+    )
 
-    # 3) Compute "isometric latitude" psi (one version of the formula)
-    #   For small lat, the simpler series expansions are also used, but let's do the direct approach:
-    #   psi = asinh(tan(lat)) - e * atanh(e * sin(lat)), etc.
-    #   We'll use a commonly cited formula for isometric latitude:
-    t = math.tan(math.pi/4 + lat/2)
-    part_e = ((1 - e*math.sin(lat)) / (1 + e*math.sin(lat)))**(e/2)
-    psi = math.log(t) - math.log(part_e)
+    northing = N0 + k0 * (
+          M
+        + N_val * math.tan(phi) * (
+              A**2 / 2
+            + (5 - T + 9*C + 4*C**2) * A**4 / 24
+            + (61 - 58*T + T**2 + 600*C - 330*ep2) * A**6 / 720
+        )
+    )
 
-    # 4) xi' and eta'
-    #   Delta lambda
-    dlon = lon - lambda0
-
-    xi_prime = math.atan2(math.sinh(psi), math.cos(dlon))
-    eta_prime = math.atanh(math.sin(dlon) / math.cosh(psi))
-
-    # 5) Apply the alpha_i sums for x, y
-    def sincosh_sum(i):
-        # sin(2 i xi'), cosh(2 i eta')
-        return math.sin(2*i*xi_prime) * math.cosh(2*i*eta_prime)
-
-    def sinsinh_sum(i):
-        # sin(2 i xi'), sinh(2 i eta')
-        return math.sin(2*i*xi_prime) * math.sinh(2*i*eta_prime)
-
-    # Summation for x (eta') and y (xi')
-    # x = k0 * A [ eta' + Σ alpha_i sin(2 i xi') cosh(2 i eta') ] + E0
-    # y = k0 * A [ xi' + Σ alpha_i sin(2 i xi') sinh(2 i eta') ] + N0
-
-    # We'll just sum up i=1..3 for demonstration:
-    sum_x = (alpha_1 * sincosh_sum(1)
-           + alpha_2 * sincosh_sum(2)
-           + alpha_3 * sincosh_sum(3))
-
-    sum_y = (alpha_1 * sinsinh_sum(1)
-           + alpha_2 * sinsinh_sum(2)
-           + alpha_3 * sinsinh_sum(3))
-
-    x = E0 + k0 * A * (eta_prime + sum_x)
-    y = N0 + k0 * A * (xi_prime + sum_y)
-
-    return (x, y)
+    return easting, northing
